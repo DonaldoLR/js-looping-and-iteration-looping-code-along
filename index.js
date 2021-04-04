7d75b55c9ab1593c341c006367eccce10eaec06b
github username <github email address>
2021-04-04 01:36:38 -0400

    reviewed for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(arrayNames, event){
+
+    const collectedMessagesArray = [];
+
+    for (let i = 0; i < arrayNames.length; i++){
+        collectedMessagesArray[i] = `Thank you, ${arrayNames[i]}, for the wonderful ${event} gift!`;
+    }
+
+    return collectedMessagesArray;
+}
+
+function countDown(int){
+
+    while (int >= 0){
+        console.log(int);
+        int--;
+    }
+}
